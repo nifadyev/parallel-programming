67f5bbdaf33ec63401ef1e21dd133bb81f6a784c
Vadim Nifadyev <vnifadyev@gmail.com>
2018-10-24 11:34:26 +0300

    Clean up the code, add some functions
    - Remove TODO and FIXME
    - Remove lots of comments and unnecessary variables
    - Rename some arrays
    -  Add functions CheckResults, PrintMatrix and MultiplyMatrices

--- 1608/nifadyev_vs/task2/matrices_multiplication_tape_horizontal_scheme.c
+++ 1608/nifadyev_vs/task2/matrices_multiplication_tape_horizontal_scheme.c
@@ -3,39 +3,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Task #18: Matrix multiplication using tape horizontal scheme (splits only first matrix into strings)
+
 void InitializeMatrix(int *matrix, const int matrixSize);
 void MultiplyMatrices(int *matrixA, int *matrixB, int *resultMatrix,
-                      const int columns, const int rows);
+                      const int l, const int m, const int n);
 void CheckResults(int *linearResultingMatrix, int *parallelResultingMatrix, const int matrixSize);
 void PrintMatrix(int *matrix, const int size, const int columns);
 
-// TODO: Not working if rows and columns are different
-// FIXME: change somewhere ROWS to COLUMNS or on the contrary
-// FIXME: Not working if ROWS or COLUMNS % procNum != 0
-// TODO: change location of startTime and endTime for par alg
-
-// TODO: breaks if ROWS < COLUMNS
-// FIXME: works correctly if ROWS >= COLUMNS
-
 int main(int argc, char **argv)
 {
+    int i, j, k;
     int A_ROWS = 0, A_COLUMNS = 0, B_ROWS = 0, B_COLUMNS = 0;
     int A_SIZE = 0, B_SIZE = 0;
-    int *matrixA = NULL, *matrixB = NULL, *linearResultingMatrix = NULL, *parallelResultingMatrix = NULL;
+    int *matrixA = NULL, *matrixB = NULL;
+    int *linearResultingMatrix = NULL, *parallelResultingMatrix = NULL;
     int procNum = 1, procRank = 0;
-
-    int elementsPerProc, rowsPerProc, remainingRows;
-    int *rowsPerProcs = NULL, *shifts = NULL;
+    int rowsPerProc = 0, remainingRows = 0;
+    int *elementsPerProc = NULL, *shifts = NULL;
     int *bufferA = NULL, *resultBuffer = NULL;
-    int i, j, k;
     double startTime = 0, endTime = 0;
     double linearTime = 0, parallelTime = 0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-
-    //FIXME: Last strings of par resulting matrix are completely wrong
 
     if (procRank == 0)
     {
@@ -54,47 +46,38 @@ int main(int argc, char **argv)
         }
         A_SIZE = A_ROWS * A_COLUMNS;
         B_SIZE = B_ROWS * B_COLUMNS;
+
         srand(time(NULL));
         matrixA = (int *)malloc(sizeof(int) * A_SIZE);
-        InitializeMatrix(matrixA, A_SIZE);
-        // printf("\nA matrix");
-        // PrintMatrix(matrixA, A_SIZE, A_COLUMNS);
         matrixB = (int *)malloc(sizeof(int) * B_SIZE);
+        InitializeMatrix(matrixA, A_SIZE);
         InitializeMatrix(matrixB, B_SIZE);
-        // printf("\nB matrix");
-        // PrintMatrix(matrixB, B_SIZE, B_COLUMNS);
-        // linearResultingMatrix = (int *)malloc(sizeof(int) * MATRIX_SIZE);
-        //parallelResultingMatrix = (int *)malloc(sizeof(int) * MATRIX_SIZE);
     }
 
-
+    //***********************Parallel Realization***********************
     startTime = MPI_Wtime();
-    //FIXME: do i really need to do this???
     MPI_Bcast(&A_ROWS, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&A_COLUMNS, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&A_SIZE, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    //MPI_Bcast(&B_ROWS, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&B_COLUMNS, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&A_SIZE, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&B_SIZE, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
     if (procRank != 0)
     {
         matrixB = (int *)malloc(sizeof(int) * B_SIZE);
     }
     MPI_Bcast(matrixB, B_SIZE, MPI_INT, 0, MPI_COMM_WORLD);
 
-    rowsPerProcs = (int *)malloc(sizeof(int) * procNum);
+    elementsPerProc = (int *)malloc(sizeof(int) * procNum);
     shifts = (int *)malloc(sizeof(int) * procNum);
     rowsPerProc = A_ROWS / procNum;
-    //elementsPerProc = rowsPerProc * COLUMNS;
     for (i = 0; i < procNum; i++)
     {
-        rowsPerProcs[i] = rowsPerProc * A_COLUMNS;
+        elementsPerProc[i] = rowsPerProc * A_COLUMNS;
         shifts[i] = i * rowsPerProc * A_COLUMNS;
     }
     remainingRows = A_ROWS - (procNum - 1) * rowsPerProc;
-    rowsPerProcs[procNum - 1] = remainingRows * A_COLUMNS;
-
-    // Parallel Realization
+    elementsPerProc[procNum - 1] = remainingRows * A_COLUMNS;
 
     if (procRank == procNum - 1)
     {
@@ -102,121 +85,62 @@ int main(int argc, char **argv)
     }
     bufferA = (int *)malloc(sizeof(int) * rowsPerProc * A_COLUMNS);
 
-    //MPI_Scatter(matrixA, rowsPerProc, MPI_INT, bufferA, rowsPerProc, MPI_INT, 0, MPI_COMM_WORLD);
-
-    MPI_Scatterv(matrixA, rowsPerProcs, shifts, MPI_INT, bufferA, rowsPerProcs[procRank], MPI_INT, 0, MPI_COMM_WORLD);
+    // Scatters a buffer in parts to all processes in a communicator
+    MPI_Scatterv(matrixA, elementsPerProc, shifts, MPI_INT, bufferA,
+                 elementsPerProc[procRank], MPI_INT, 0, MPI_COMM_WORLD);
 
     resultBuffer = (int *)malloc(sizeof(int) * rowsPerProc * B_COLUMNS);
-    //MultiplyMatrices(bufferA, matrixB, resultBuffer, /* ROWS */ COLUMNS, rowsPerProc /* rowsPerProc, ROWS */);
 
-    for (i = 0; i < rowsPerProc; i++)
-    {
-        for (j = 0; j < B_COLUMNS; j++)
-        {
-            resultBuffer[i * B_COLUMNS + j] = 0;
-            for (k = 0; k < A_COLUMNS; k++)
-            {
-                resultBuffer[i * B_COLUMNS + j] += bufferA[i * A_COLUMNS + k] * matrixB[k * B_COLUMNS + j];
-            }
-        }
-    }
+    MultiplyMatrices(bufferA, matrixB, resultBuffer, rowsPerProc, A_COLUMNS, B_COLUMNS);
+
     if (procRank == 0)
     {
         parallelResultingMatrix = (int *)malloc(sizeof(int) * A_ROWS * B_COLUMNS);
-        // for(i = 0; i < A_ROWS * B_COLUMNS; i++)
-        // {
-        //     parallelResultingMatrix[i] = 0;
-        // }
     }
-    rowsPerProc = A_ROWS / procNum;
-    //elementsPerProc = rowsPerProc * /* ROWS */ COLUMNS;
-    for (i = 0; i < procNum; i++) //maybe < procNum -1
+
+    for (i = 0; i < procNum; i++)
     {
-        rowsPerProcs[i] = rowsPerProc * B_COLUMNS;
+        elementsPerProc[i] = rowsPerProc * B_COLUMNS;
         shifts[i] = i * rowsPerProc * B_COLUMNS;
     }
-    //remainingRows = /* ROWS % procNum */ ROWS - (procNum - 1) * rowsPerProc;
-    rowsPerProcs[procNum - 1] = remainingRows * B_COLUMNS;
-
-    //MPI_Gather(resultBuffer, rowsPerProc * COLUMNS, MPI_INT, parallelResultingMatrix, rowsPerProc * COLUMNS, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Gatherv(resultBuffer, rowsPerProcs[procRank], MPI_INT, parallelResultingMatrix, rowsPerProcs, shifts, MPI_INT, 0, MPI_COMM_WORLD);
+    elementsPerProc[procNum - 1] = remainingRows * B_COLUMNS;
 
     // Gathers together values from a group of processes
+    MPI_Gatherv(resultBuffer, elementsPerProc[procRank], MPI_INT,
+                parallelResultingMatrix, elementsPerProc, shifts, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (procRank == 0)
     {
         endTime = MPI_Wtime();
-        //CheckResults(linearResultingMatrix, parallelResultingMatrix, MATRIX_SIZE);
-        // Linear Realization
-        // printf("\nPARALLEL RESULT");
-        // for (i = 0; i < MATRIX_SIZE; i++)
-        // {
-        //     if (i % COLUMNS == 0)
-        //     {
-        //         printf("\n");
-        //     }
-        //     printf("%d ", parallelResultingMatrix[i]);
-        // }
-        // printf("\n\n");
-        // printf("\nPARALLEL RESULT");
-        // PrintMatrix(parallelResultingMatrix, A_ROWS * B_COLUMNS, B_COLUMNS);
         parallelTime = endTime - startTime;
         printf("Parallel time = %.3f\n", parallelTime);
-        // if (procRank == 0)
-        // {
+        //****************************************************************
+
+        //***********************Linear Realization***********************
         linearResultingMatrix = (int *)malloc(sizeof(int) * A_ROWS * B_COLUMNS);
+
         startTime = MPI_Wtime();
-        //MultiplyMatrices(matrixA, matrixB, linearResultingMatrix, COLUMNS, ROWS);
-        for (i = 0; i < A_ROWS; i++)
-        {
-            for (j = 0; j < B_COLUMNS; j++)
-            {
-                linearResultingMatrix[i * B_COLUMNS + j] = 0;
-                for (k = 0; k < A_COLUMNS; k++)
-                {
-                    linearResultingMatrix[i * B_COLUMNS + j] += matrixA[i * A_COLUMNS + k] * matrixB[k * B_COLUMNS + j];
-                }
-            }
-        }
+        MultiplyMatrices(matrixA, matrixB, linearResultingMatrix, A_ROWS, A_COLUMNS, B_COLUMNS);
         endTime = MPI_Wtime();
         linearTime = endTime - startTime;
-        // printf("LINEAR RESULT");
-        // PrintMatrix(linearResultingMatrix, A_ROWS * B_COLUMNS, B_COLUMNS);
-
-        // printf("LINEAR RESULT");
-        // for (i = 0; i < MATRIX_SIZE; i++)
-        // {
-        //     if (i % COLUMNS == 0)
-        //     {
-        //         printf("\n");
-        //     }
-        //     printf("%d ", linearResultingMatrix[i]);
-        // }
-        // printf("\n\n");
         printf("Linear time = %.3f\n\n", linearTime);
-        //}
-        for (i = 0; i < A_ROWS * B_COLUMNS; i++)
-        {
-            if (linearResultingMatrix[i] != parallelResultingMatrix[i])
-            {
-                printf("Error! Linear and parallel results are not equal\n");
-            }
-        }
+        //****************************************************************
+
+        CheckResults(linearResultingMatrix, parallelResultingMatrix, A_ROWS * B_COLUMNS);
+        printf("Performance difference: %.3f%\n", (linearTime / parallelTime) * 100);
     }
 
-    if (procRank == 0)
-    {
-        free(matrixA);
-        free(linearResultingMatrix);
-        free(parallelResultingMatrix);
-    }
     MPI_Finalize();
-    free(matrixB);
 
+    free(matrixA);
+    free(matrixB);
     free(bufferA);
     free(resultBuffer);
-    free(rowsPerProcs);
+    free(elementsPerProc);
     free(shifts);
+    free(linearResultingMatrix);
+    free(parallelResultingMatrix);
+
     return 0;
 }
 
@@ -231,18 +155,18 @@ void InitializeMatrix(int *matrix, const int matrixSize)
 }
 
 void MultiplyMatrices(int *matrixA, int *matrixB, int *resultMatrix,
-                      const int columns, const int rows)
+                      const int l, const int m, const int n)
 {
     int i, j, k;
 
-    for (i = 0; i < /* columns */ rows; i++)
+    for (i = 0; i < l; i++)
     {
-        for (j = 0; j < /* rows */ columns; j++)
+        for (j = 0; j < n; j++)
         {
-            resultMatrix[i * /* rows */ columns + j] = 0;
-            for (k = 0; k < /* rows */ columns; k++)
+            resultMatrix[i * n + j] = 0;
+            for (k = 0; k < m; k++)
             {
-                resultMatrix[i * /* rows */ columns + j] += matrixA[i * /* rows */ columns + k] * matrixB[k * /* rows */ columns + j];
+                resultMatrix[i * n + j] += matrixA[i * m + k] * matrixB[k * n + j];
             }
         }
     }
