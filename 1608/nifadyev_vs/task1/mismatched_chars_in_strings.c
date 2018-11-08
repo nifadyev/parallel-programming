cd0b169641c3e2922fbb03b3f461fee726e978db
Vadim Nifadyev <vnifadyev@gmail.com>
2018-11-08 11:15:43 +0300

    Fix memory leak, remove block of code to proc 0

--- 1608/nifadyev_vs/task1/mismatched_chars_in_strings.c
+++ 1608/nifadyev_vs/task1/mismatched_chars_in_strings.c
@@ -18,22 +18,18 @@ int main(int argc, char **argv)
     int linearResult = 0, parallelResult = 0, mismatchCount = 0;
     double startTime = 0, endTime = 0;
     double linearTime = 0, parallelTime = 0;
-    char *str1, *str2;
-    char *temp1, *temp2;
+    char *str1 = NULL, *str2 = NULL;
+    char *temp1 = NULL, *temp2 = NULL;
     MPI_Status status;
 
-    if (argc == 1)
-    {
-        MAX_SIZE = 10000000;
-    }
-    else
+    if (argc == 2)
     {
         MAX_SIZE = atoi(argv[1]);
     }
-
-    str1 = (char *)malloc(sizeof(char) * (MAX_SIZE + 1));
-    str2 = (char *)malloc(sizeof(char) * (MAX_SIZE + 1));
-    InitializeStrings(str1, str2, MAX_SIZE);
+    else
+    {
+        MAX_SIZE = 10000000;
+    }
 
     MPI_Init(&argc, &argv);
     // Get the number of processors
@@ -47,6 +43,9 @@ int main(int argc, char **argv)
     /************Linear realization************/
     if (procRank == 0)
     {
+        str1 = (char *)malloc(sizeof(char) * (MAX_SIZE + 1));
+        str2 = (char *)malloc(sizeof(char) * (MAX_SIZE + 1));
+        InitializeStrings(str1, str2, MAX_SIZE);
         startTime = MPI_Wtime();
         linearResult = CountMismatchesInTwoStrings(str1, str2, MAX_SIZE);
         endTime = MPI_Wtime();
@@ -54,11 +53,9 @@ int main(int argc, char **argv)
 
         printf("Linear time = %.3f\n", endTime - startTime);
         printf("Linear result = %d\n", linearResult);
-    }
 
-    /************Parallel realization********/
-    if (procRank == 0)
-    {
+        /************Parallel realization********/
+
         startTime = MPI_Wtime();
 
         partRemainderFlag = 0;
@@ -89,6 +86,8 @@ int main(int argc, char **argv)
         MPI_Recv(temp1, partSize + partRemainder * partRemainderFlag, MPI_CHAR, 0, FIRST_STRING, MPI_COMM_WORLD, &status);
         MPI_Recv(temp2, partSize + partRemainder * partRemainderFlag, MPI_CHAR, 0, SECOND_STRING, MPI_COMM_WORLD, &status);
         mismatchCount = CountMismatchesInTwoStrings(temp1, temp2, partSize + partRemainder * partRemainderFlag);
+        free(temp1);
+        free(temp2);
     }
 
     // Collect the result into main processor
@@ -106,11 +105,11 @@ int main(int argc, char **argv)
         printf("MPI time = %.3f\n", endTime - startTime);
         printf("MPI result = %d\n", parallelResult);
         printf("Performance difference: %.1f\%\n", ((linearTime - parallelTime) / ((linearTime + parallelTime) / 2)) * 100);
+        free(str1);
+        free(str2);
     }
 
     MPI_Finalize();
-    free(str1);
-    free(str2);
 
     return 0;
 }
