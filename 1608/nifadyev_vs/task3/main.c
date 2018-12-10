c1cf333023aa09653957ab56096bddf1d141bf95
Vadim Nifadyev <vnifadyev@gmail.com>
2018-12-10 16:17:53 +0300

    Add bugged parallel algorithm

--- 1608/nifadyev_vs/task3/main.c
+++ 1608/nifadyev_vs/task3/main.c
@@ -1,9 +1,11 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// #include <mpich/mpi.h>
+#include <math.h>
 #include "shell_sort.h"
 
+//TODO: move parallel code into separate function
+//TODO: to consider remainder need to use SCATTERV
+//TODO: if possible get rid of pow and log10
+//TODO: move parallel resulting array from subArrayy to parallelResultingArray
+//FIXME: not working if procNum % 2 == 1
 
 int main(int argc, char **argv)
 {
@@ -12,7 +14,11 @@ int main(int argc, char **argv)
     int length;
     int *array;
     int *linearResultingArray, *parallelResultingArray;
+    int *subArrayPerProc;
+    int *mergedArray;
+    int *buffer;
     double startTime, endTime;
+    int elementsPerProc;
     MPI_Status status;
 
     MPI_Init(&argc, &argv);
@@ -25,12 +31,20 @@ int main(int argc, char **argv)
 
         linearResultingArray = (int *)malloc(sizeof(int) * length);
         GenerateArray(linearResultingArray, length);
-        // CopyArray(linearResultingArray, parallelResultingArray, length);
+    }
 
+    MPI_Bcast(&length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    parallelResultingArray = (int *)malloc(sizeof(int) * length);
+
+    if (procRank == 0)
+    {
         if (length < 30)
         {
             PrintArray(linearResultingArray, length);
         }
+
+        // parallelResultingArray = (int *)malloc(sizeof(int) * length);
+        CopyArray(linearResultingArray, parallelResultingArray, length);
 
         startTime = MPI_Wtime();
         ShellSort(linearResultingArray, length);
@@ -41,8 +55,74 @@ int main(int argc, char **argv)
             PrintArray(linearResultingArray, length);
         }
         printf("Linear time: %.4f\n", endTime - startTime);
+
+        elementsPerProc = length / procNum;
+
         free(linearResultingArray);
     }
 
+    MPI_Bcast(&elementsPerProc, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    subArrayPerProc = (int *)malloc(sizeof(int) * elementsPerProc);
+
+    MPI_Scatter(parallelResultingArray, elementsPerProc, MPI_INT,
+                subArrayPerProc, elementsPerProc, MPI_INT, 0,
+                MPI_COMM_WORLD);
+
+    ShellSort(subArrayPerProc, elementsPerProc);
+
+    // int iteration = log10(procNum) / log10(2);
+    int iteration = procNum / 2;
+    if (procRank == 0)
+    {
+        printf("iteration: %d\n", iteration);
+    }
+
+    for (i = 0; i < iteration; i++)
+    {
+        //TODO: what does it do?
+        if ((procRank % ((int)pow(2, i + 1))) == 0)
+        {
+            printf("i: %d, pow: %d\n", i, (int)pow(2, i + 1));
+            mergedArray = (int *)malloc(sizeof(int) * elementsPerProc * 2);
+            buffer = (int *)malloc(sizeof(int) * elementsPerProc);
+
+            //FIXME: if procNum % 2 == 1, source procRank == procNum, thats incorrect
+            MPI_Recv(buffer, elementsPerProc, MPI_INT, procRank + (int)pow(2, i), i, MPI_COMM_WORLD, &status);
+
+            Merge(subArrayPerProc, elementsPerProc, buffer, elementsPerProc, mergedArray);
+
+            free(subArrayPerProc);
+
+            subArrayPerProc = (int *)malloc(sizeof(int) * elementsPerProc * 2);
+            // subArrayPerProc = realloc(subArrayPerProc, sizeof(int) * elementsPerProc * 2);
+            CopyArray(mergedArray, subArrayPerProc, elementsPerProc * 2);
+
+            elementsPerProc *= 2;
+
+            free(mergedArray);
+            free(buffer);
+        }
+        else if (procRank % (int)pow(2, i) == 0)
+        {
+            printf("i: %d, pow: %d\n", i, (int)pow(2, i));
+            MPI_Send(subArrayPerProc, elementsPerProc, MPI_INT, procRank - (int)pow(2, i), i, MPI_COMM_WORLD);
+        }
+    }
+
+    if (procRank == 0)
+    {
+        printf("subArray: ");
+        PrintArray(subArrayPerProc, elementsPerProc);
+        printf("\nArray: ");
+        PrintArray(parallelResultingArray, length);
+
+        // AreResultsEqual(linearResultingArray, parallelResultingArray, length);
+
+        free(parallelResultingArray);
+    }
+
+    free(subArrayPerProc);
+
+    MPI_Finalize();
     return 0;
 }
