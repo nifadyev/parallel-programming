e5603c54cfa64adb11a9a8067c83058deb845b73
Vadim Nifadyev <vnifadyev@gmail.com>
2018-12-10 19:58:51 +0300

    Move parallel algorithm to function,  clean up code

--- 1608/nifadyev_vs/task3/main.c
+++ 1608/nifadyev_vs/task3/main.c
@@ -1,30 +1,12 @@
-#include <math.h>
 #include "shell_sort.h"
-
-//TODO: move parallel code into separate function
-//TODO: to consider remainder need to use SCATTERV
-//TODO: if possible get rid of pow and log10
-//TODO: move parallel resulting array from subArrayy to parallelResultingArray
-//FIXME: not working if procNum % 2 == 1
 
 int main(int argc, char **argv)
 {
     int procRank, procNum;
-    int i, j;
     int length;
-    // int *array;
     int *linearResultingArray, *parallelResultingArray;
-    int *subArrayPerProc;
-    int *mergedArray;
-    int *buffer;
-    int *other;
-    int *shifts, *elemsPerProc;
-    int m; // tempElementsPerProc
-    int near;
     double startTime, endTime;
-    int elementsPerProc, remaindedElements;
-    int step;
-    MPI_Status status;
+    double linearTime, parallelTime;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
@@ -36,22 +18,23 @@ int main(int argc, char **argv)
 
         linearResultingArray = (int *)malloc(sizeof(int) * length);
         GenerateArray(linearResultingArray, length);
+
+        parallelResultingArray = (int *)malloc(sizeof(int) * length);
     }
 
-    MPI_Bcast(&length, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    shifts = (int *)malloc(sizeof(int) * procNum);
-    elemsPerProc = (int *)malloc(sizeof(int) * procNum);
-    // parallelResultingArray = (int *)malloc(sizeof(int) * length);
+    ParallelShellSort(linearResultingArray, length, procNum, procRank,
+                        parallelTime, parallelResultingArray);
 
     if (procRank == 0)
     {
         if (length < 30)
         {
+            printf("Initial array: ");
             PrintArray(linearResultingArray, length);
-        }
 
-        parallelResultingArray = (int *)malloc(sizeof(int) * length);
-        CopyArray(linearResultingArray, parallelResultingArray, length);
+            printf("Sorted array (parallel algorithm): ");
+            PrintArray(parallelResultingArray, length);
+        }
 
         startTime = MPI_Wtime();
         ShellSort(linearResultingArray, length);
@@ -59,94 +42,23 @@ int main(int argc, char **argv)
 
         if (length < 30)
         {
+            printf("Sorted array (linear algorithm):   ");
             PrintArray(linearResultingArray, length);
         }
-        printf("Linear time: %.4f\n", endTime - startTime);
 
-        elementsPerProc = length / procNum;
-        // remaindedElements = length % procNum;
-        // if (remaindedElements != 0)
-        // {
-        //     elementsPerProc++;
-        // }
-        // elementsPerProc += (length % procNum != 0) ? 1 : 0;
+        linearTime = endTime - startTime;
 
-        for (i = 0; i < procNum; i++)
+        printf("Linear time:   %.4f\n", linearTime);
+        printf("Parallel time: %.4f\n", parallelTime);
+
+        if (AreResultsEqual(linearResultingArray, parallelResultingArray, length))
         {
-            shifts[i] = i * elementsPerProc;
-            elemsPerProc[i] = elementsPerProc;
+            printf("Results are not equal\n");
         }
-        // shifts[procNum - 1] = elementsPerProc + length % procNum;
-        elemsPerProc[procNum - 1] = elementsPerProc + length % procNum;
 
         free(linearResultingArray);
-    }
-
-    MPI_Bcast(shifts, procNum, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(elemsPerProc, procNum, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&elementsPerProc, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    if (procRank == procNum - 1)
-    {
-        elementsPerProc += length % procNum;
-    }
-    // MPI_Bcast(&remaindedElements, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    subArrayPerProc = (int *)malloc(sizeof(int) * elementsPerProc);
-
-    // MPI_Scatter(parallelResultingArray, elementsPerProc, MPI_INT,
-    //             subArrayPerProc, elementsPerProc, MPI_INT, 0,
-    //             MPI_COMM_WORLD);
-    MPI_Scatterv(parallelResultingArray, elemsPerProc, shifts,
-                MPI_INT, subArrayPerProc, elemsPerProc[procRank],
-                MPI_INT, 0, MPI_COMM_WORLD);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    ShellSort(subArrayPerProc, elementsPerProc);
-
-    step = 1;
-    while (step < procNum)
-    {
-        if (procRank % (2 * step) == 0)
-        {
-            if (procRank + step < procNum)
-            {
-                MPI_Recv(&m, 1, MPI_INT, procRank + step, 0, MPI_COMM_WORLD, &status);
-                other = (int *)malloc(m * sizeof(int));
-                MPI_Recv(other, m, MPI_INT, procRank + step, 1, MPI_COMM_WORLD, &status);
-                // printf("%d received: ", step);
-                // PrintArray(other, m);
-                // PrintArray(subArrayPerProc, elementsPerProc);
-                subArrayPerProc = Merge(subArrayPerProc, elementsPerProc, other, m);
-                // PrintArray(subArrayPerProc, elementsPerProc);
-                elementsPerProc += m;
-            }
-        }
-        else
-        {
-            near = procRank - step;
-            MPI_Send(&elementsPerProc, 1, MPI_INT, near, 0, MPI_COMM_WORLD);
-            MPI_Send(subArrayPerProc, elementsPerProc, MPI_INT, near, 1, MPI_COMM_WORLD);
-            // printf("%d sent: ", step);
-            // PrintArray(subArrayPerProc, elementsPerProc);
-            break;
-        }
-        step *= 2;
-    }
-
-    if (procRank == 0)
-    {
-        printf("subArray: ");
-        PrintArray(subArrayPerProc, elementsPerProc);
-        // printf("Array: ");
-        // PrintArray(parallelResultingArray, length);
-
-        // AreResultsEqual(linearResultingArray, parallelResultingArray, length);
-
         free(parallelResultingArray);
     }
-
-    // free(subArrayPerProc);
-    // free(other);
 
     MPI_Finalize();
     return 0;
--- 1608/nifadyev_vs/task3/shell_sort.h
+++ 1608/nifadyev_vs/task3/shell_sort.h
@@ -35,40 +35,11 @@ void CopyArray(int *sourceArray, int *destinationArray, const int length)
     }
 }
 
-// void ShellSort(int *array, const int length)
-// {
-//     // int gap = length / 2;  //
-//     int gap, i, j; //
-//     int temp;
-
-//     for (gap = length / 2; gap > 0; gap /= 2)
-//     {
-//         // Do a gapped insertion sort for this gap size.
-//         // The first gap elements a[0..gap-1] are already in gapped order
-//         // keep adding one more element until the entire array is
-//         // gap sorted
-//         for (i = gap; i < length; i++)
-//         {
-//             // add a[i] to the elements that have been gap sorted
-//             // save a[i] in temp and make a hole at position i
-//             temp = array[i];
-//             // shift earlier gap-sorted elements up until the correct
-//             // location for a[i] is found
-//             for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-//             {
-//                 array[j] = array[j - gap];
-//             }
-//             //  put temp (the original a[i]) in its correct location
-//             array[j] = temp;
-//         }
-//     }
-// }
-
 void ShellSort(int *array, const int length)
 {
     int gap = length / 2;
     int i, j, k, temp;
-    while(gap > 0)
+    while (gap > 0)
     {
         for (i = gap; i < length; i++)
         {
@@ -93,7 +64,7 @@ void ShellSort(int *array, const int length)
     }
 }
 
-void Merge(int *firstArray, const int firstArrayLength, int * secondArray, const int secondArrayLength, int *resultArray)
+void Merge(int *firstArray, const int firstArrayLength, int *secondArray, const int secondArrayLength, int *resultArray)
 {
     int i, k = 0, j = 0;
     for (i = 0; i < firstArrayLength + secondArrayLength; i++)
@@ -121,7 +92,7 @@ void Merge(int *firstArray, const int firstArrayLength, int * secondArray, const
     }
 }
 
-int *Merge(int *firstArray, const int firstArrayLength, int * secondArray, const int secondArrayLength)
+int *Merge(int *firstArray, const int firstArrayLength, int *secondArray, const int secondArrayLength)
 {
     int i, k = 0, j = 0;
     int *resultArray = (int *)malloc(sizeof(int) * (firstArrayLength + secondArrayLength));
@@ -165,4 +136,87 @@ int AreResultsEqual(int *linearResultingArray, int *parallelResultingArray, cons
     }
 
     return 0;
+}
+
+void ParallelShellSort(int *array, int length, const int procNum,
+                       const int procRank, double &time, int *resultingArray)
+{
+    int i;
+    int *subArrayPerProc;
+    int *receivedSubArray;
+    int *subArrayShifts, *subArrayLengths;
+    int receivedSubArrayLength;
+    double startTime, endTime;
+    int elementsPerProc;
+    int step; //
+    MPI_Status status;
+
+    subArrayShifts = (int *)malloc(sizeof(int) * procNum);
+    subArrayLengths = (int *)malloc(sizeof(int) * procNum);
+
+    if (procRank == 0)
+    {
+        elementsPerProc = length / procNum;
+
+        for (i = 0; i < procNum; i++)
+        {
+            subArrayShifts[i] = i * elementsPerProc;
+            subArrayLengths[i] = elementsPerProc;
+        }
+        subArrayLengths[procNum - 1] = elementsPerProc + length % procNum;
+    }
+
+    startTime = MPI_Wtime();
+    MPI_Bcast(&length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&elementsPerProc, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(subArrayShifts, procNum, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(subArrayLengths, procNum, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (procRank == procNum - 1)
+    {
+        elementsPerProc += length % procNum;
+    }
+
+    subArrayPerProc = (int *)malloc(sizeof(int) * elementsPerProc);
+
+    MPI_Scatterv(array, subArrayLengths, subArrayShifts,
+                 MPI_INT, subArrayPerProc, subArrayLengths[procRank],
+                 MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    free(subArrayShifts);
+    free(subArrayLengths);
+
+    ShellSort(subArrayPerProc, elementsPerProc);
+
+    for (step = 1; step < procNum; step *= 2)
+    {
+        if (procRank % (2 * step) == 0)
+        {
+            if (procRank + step < procNum)
+            {
+                MPI_Recv(&receivedSubArrayLength, 1, MPI_INT, procRank + step, 0, MPI_COMM_WORLD, &status);
+                receivedSubArray = (int *)malloc(receivedSubArrayLength * sizeof(int));
+                MPI_Recv(receivedSubArray, receivedSubArrayLength, MPI_INT, procRank + step, 1, MPI_COMM_WORLD, &status);
+                subArrayPerProc = Merge(subArrayPerProc, elementsPerProc, receivedSubArray, receivedSubArrayLength);
+                free(receivedSubArray);
+                elementsPerProc += receivedSubArrayLength;
+            }
+        }
+        else
+        {
+            MPI_Send(&elementsPerProc, 1, MPI_INT, procRank - step, 0, MPI_COMM_WORLD);
+            MPI_Send(subArrayPerProc, elementsPerProc, MPI_INT, procRank - step, 1, MPI_COMM_WORLD);
+        }
+    }
+
+    if (procRank == 0)
+    {
+        CopyArray(subArrayPerProc, resultingArray, length);
+        endTime = MPI_Wtime();
+        time = endTime - startTime;
+    }
+
+    free(subArrayPerProc);
 }
